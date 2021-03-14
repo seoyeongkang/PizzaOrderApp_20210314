7f866c268dfecacea120037fd787544de511aece
seoyeongkang <jumprope123@naver.com>
2021-03-14 10:45:11 +0900

    BaseActivity 상속 받로고 Main 변경

--- app/src/main/java/com/tjoeun/pizzaorderapp_20210314/MainActivity.kt
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/MainActivity.kt
@@ -3,9 +3,22 @@ package com.tjoeun.pizzaorderapp_20210314
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+
     }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
 }
