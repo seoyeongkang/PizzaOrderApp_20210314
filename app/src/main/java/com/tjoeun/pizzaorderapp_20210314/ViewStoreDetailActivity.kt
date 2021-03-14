3cb31760b834a540d21fc903e797e673d4887555
seoyeongkang <jumprope123@naver.com>
2021-03-14 12:37:02 +0900

    가게 상세화면 UI 작업

---
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/ViewStoreDetailActivity.kt
@@ -0,0 +1,23 @@
+package com.tjoeun.pizzaorderapp_20210314
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
