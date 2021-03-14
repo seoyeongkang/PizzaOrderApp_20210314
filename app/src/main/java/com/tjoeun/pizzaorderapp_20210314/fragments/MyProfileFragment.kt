08c9ed9731891f6ded53381372782ffc676a8b74
seoyeongkang <jumprope123@naver.com>
2021-03-14 14:04:48 +0900

    버튼 눌리면 => 닉네임 가지러 이동

--- app/src/main/java/com/tjoeun/pizzaorderapp_20210314/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/fragments/MyProfileFragment.kt
@@ -1,13 +1,18 @@
 package com.tjoeun.pizzaorderapp_20210314.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.tjoeun.pizzaorderapp_20210314.EditNicknameActivity
 import com.tjoeun.pizzaorderapp_20210314.R
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class MyProfileFragment : Fragment() {
+
+    val REQUEST_FOR_NICKNAME = 54321
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +24,12 @@ class MyProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        editNickBtn.setOnClickListener {
 
+            val myIntent = Intent(activity, EditNicknameActivity::class.java)
+
+            startActivityForResult(myIntent, REQUEST_FOR_NICKNAME)
+
+        }
     }
 }
