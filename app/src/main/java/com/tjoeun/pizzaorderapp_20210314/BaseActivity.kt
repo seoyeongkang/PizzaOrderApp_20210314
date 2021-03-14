a7e7ce9d29dbfc60edd64134929e58b5faec09df
seoyeongkang <jumprope123@naver.com>
2021-03-14 10:42:38 +0900

    BaseActivity 추가작업

---
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.pizzaorderapp_20210314
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
