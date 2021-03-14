c357190a248ba56400b5801ef7187280cfbc33bc
seoyeongkang <jumprope123@naver.com>
2021-03-14 14:21:01 +0900

    메인 액티비티 주석 추가

--- app/src/main/java/com/tjoeun/pizzaorderapp_20210314/MainActivity.kt
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/MainActivity.kt
@@ -7,6 +7,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
+//    뷰페이저에 프래그먼트를 뿌려주기 위한 어댑터 변수
+//    lateinit var로 변수를 멤버변수 => 대임은 oncreate 이후 (setValues)에서 진행
+
     lateinit var mvpa : MyViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,10 +26,13 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+//        뷰페이저에 연결될 어댑터를 실제 객체화(메모리 등록)
         mvpa = MyViewPagerAdapter(supportFragmentManager)
 
+//        메인 화면에 있는 myViewPager의 adapter로 mvpa를 등록
         myViewPager.adapter = mvpa
 
+//        탭레이아웃을 => myViewPager와 연결 : 현재 보는 탭 표시 등등 지원
         myTabLayout.setupWithViewPager(myViewPager)
 
     }
