72e6167d70786a510d2e6c2ad5e25a896d3f2c52
seoyeongkang <jumprope123@naver.com>
2021-03-14 13:59:49 +0900

    닉네임 변경 화면 추가

---
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/EditNicknameActivity.kt
@@ -0,0 +1,19 @@
+package com.tjoeun.pizzaorderapp_20210314
+
+import android.os.Bundle
+
+class EditNicknameActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
