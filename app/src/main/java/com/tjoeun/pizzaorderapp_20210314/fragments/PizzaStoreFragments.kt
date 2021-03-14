b68a053e3d8a7f9127c71b258fc58c68bb078ee6
seoyeongkang <jumprope123@naver.com>
2021-03-14 11:07:21 +0900

    피자가게 목록 프래그먼트 추가

---
+++ app/src/main/java/com/tjoeun/pizzaorderapp_20210314/fragments/PizzaStoreFragments.kt
@@ -0,0 +1,24 @@
+package com.tjoeun.pizzaorderapp_20210314.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tjoeun.pizzaorderapp_20210314.R
+
+class PizzaStoreFragments : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_pizza_stores, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+}
